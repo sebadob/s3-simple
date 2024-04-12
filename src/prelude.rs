995011d273a12315788b2a7fb55855af14cdc4a7
sd <sebastiandobe@mailbox.org>
2024-04-12 21:19:54 +0200

    remove `prelude` and expose in root crate

--- src/prelude.rs
+++
@@ -1,4 +0,0 @@
-pub use crate::bucket::{Bucket, BucketOptions};
-pub use crate::credentials::{AccessKeyId, AccessKeySecret, Credentials};
-pub use crate::error::S3Error;
-pub use crate::types::{HeadObjectResult, Object, PutStreamResponse};
