5a4bf6e665001db35d0de81d1ee7351efb23a484
sd <sebastiandobe@mailbox.org>
2024-04-16 19:08:31 +0200

    update readme and examples with simplified API

--- examples/bucket-ops/src/main.rs
+++ examples/bucket-ops/src/main.rs
@@ -22,14 +22,14 @@ async fn main() -> Result<(), S3Error> {
     let content = b"Hello to S3";
 
     // upload
-    let res = bucket.put(file_name, content).await?;
-    assert!(res.status().is_success());
+    bucket.put(file_name, content).await?;
+    // no manual status code checking necessary,
+    // any non-success will return an S3Error
 
     // GET it back
     // The `S3Response` return is a wrapper around `reqwest::Response` so you can
     // do whatever you like with it. Either read it fully in memory or stream it.
     let res = bucket.get(file_name).await?;
-    assert!(res.status().is_success());
     let body = res.bytes().await?;
     assert_eq!(body.as_ref(), content);
 
@@ -55,21 +55,17 @@ async fn main() -> Result<(), S3Error> {
 
     // S3 internal copy -> no need to download and upload again
     let copy_file_name = "test_copy.txt";
-    let res = bucket.copy_internal(file_name, copy_file_name).await?;
-    assert!(res.is_success());
+    bucket.copy_internal(file_name, copy_file_name).await?;
 
     // GET the new copied object
     let res = bucket.get(copy_file_name).await?;
-    assert!(res.status().is_success());
     let body = res.bytes().await?;
     // the copied file should have the exact same content as our original
     assert_eq!(body.as_ref(), content);
 
     // clean up and delete the test file
-    let res = bucket.delete(file_name).await?;
-    assert!(res.status().is_success());
-    let res = bucket.delete(copy_file_name).await?;
-    assert!(res.status().is_success());
+    bucket.delete(file_name).await?;
+    bucket.delete(copy_file_name).await?;
 
     Ok(())
 }
